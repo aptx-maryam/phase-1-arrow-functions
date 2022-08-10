3c2cdac088724bd5707c9f519a7c14b03b35fe1b
maryamhaitham <marym.ihnj@gmail.com>
2022-08-10 17:10:18 +0300

    Done

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// has a function called divide, divide divides 2000 by 100
+const divide = function(){
+    return 2000/100
+};
+
+//has an arrow function called square, 
+//square arrow function takes one parameter and multiplies it times itself
+const square = number => number * 2;
+
+//has an arrow function called add
+//add arrow function takes two parameters and adds them together
+const add = (a, b) => a + b;
